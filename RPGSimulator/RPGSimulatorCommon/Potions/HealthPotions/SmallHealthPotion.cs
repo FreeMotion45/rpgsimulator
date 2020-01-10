646ba0482d4f583e38228ac4254e59d1341ccc34
FreeMotion45 <thewebsiter@gmail.com>
2020-01-10 17:44:12 +0200

    Fixed naming and added small hp pot

---
+++ RPGSimulator/RPGSimulatorCommon/Potions/HealthPotions/SmallHealthPotion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGSimulatorCommon.Potions.HealthPotions
+{
+    class SmallHealthPotion : HealthPotionBase
+    {
+        protected override int CalculateHealingAmount()
+        {
+            return 20;
+        }
+    }
+}
