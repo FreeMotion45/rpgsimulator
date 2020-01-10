d388a48fea83b1810ff3c00c2e5c570a482c7286
FreeMotion45 <thewebsiter@gmail.com>
2020-01-10 18:01:10 +0200

    Created game class

---
+++ RPGSimulator/RPGSimulatorCommon/Game.cs
@@ -0,0 +1,33 @@
+﻿using RPGSimulatorCommon.Character;
+using RPGSimulatorCommon.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGSimulatorCommon
+{
+    class Game : IGameController
+    {
+        public Game(CharacterState you, CharacterState enemy)
+        {
+            You = you;
+            Enemy = enemy;
+        }
+
+        public CharacterState You { get; }
+
+        public CharacterState Enemy { get; }
+
+        public void UsePotion()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UseSkill()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
