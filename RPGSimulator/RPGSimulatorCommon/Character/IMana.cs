fcb4232ecdd0bf0ac4dbcf4c2c84dd6f0e387340
FreeMotion45 <thewebsiter@gmail.com>
2020-01-11 01:48:11 +0200

    Made IHealth and IMana

---
+++ RPGSimulator/RPGSimulatorCommon/Character/IMana.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGSimulatorCommon.Character
+{
+    interface IMana
+    {
+        int MaxMana { get; internal set; }
+        int CurrentMana { get; internal set; }
+    }
+}
