fe70348c9e9c4218406363b4d250c6e1d01a8e10
FreeMotion45 <thewebsiter@gmail.com>
2020-01-11 01:27:05 +0200

    Made IJob

---
+++ RPGSimulator/RPGSimulatorCommon/Character/Jobs/IJob.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGSimulatorCommon.Character.Jobs
+{
+    interface IJob
+    {
+        void Attack(ICharacter target);
+        void UseSpecialAbility(ICharacter target);
+        void AddBonusAttributes(ICharacter self);
+    }
+}
---
+++ RPGSimulator/RPGSimulatorCommon/Character/Jobs/Warrior.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGSimulatorCommon.Character.Jobs
+{
+    class Warrior : IJob
+    {
+        public readonly int _minimumDamage;
+        public readonly int _maximumDamage;
+        public readonly Random random;
+
+        public Warrior()
+        {
+            _minimumDamage = 9;
+            _maximumDamage = 15;
+            random = new Random();
+        }
+
+        public void AddBonusAttributes(ICharacter self)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Attack(ICharacter target)
+        {
+            target.Health.CurrentHealth -= CalculateAttackDamage(); 
+        }
+
+        public void UseSpecialAbility(ICharacter target)
+        {
+            throw new NotImplementedException();
+        }
+
+        private int CalculateAttackDamage()
+        {
+            return random.Next(_minimumDamage, _maximumDamage);
+        }
+    }
+}
