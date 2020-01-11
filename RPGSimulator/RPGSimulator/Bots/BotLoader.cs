a6013b06e9de28ccce8bc3da604b23858018f6ed
FreeMotion45 <thewebsiter@gmail.com>
2020-01-11 21:38:14 +0200

    Made BotLoader (not implemented yet)

---
+++ RPGSimulator/RPGSimulator/Bots/BotLoader.cs
@@ -0,0 +1,16 @@
+﻿using RPGSimulator.Common.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestBot;
+
+namespace RPGSimulator.Bots
+{
+    class BotLoader
+    {
+        public List<IBot> LoadBots()
+        {
+            return new List<IBot>() { new BotSean(), new BotEmanuel() };
+        }
+    }
+}
---
+++ RPGSimulator/TestBot/BotEmanuel.cs
@@ -0,0 +1,23 @@
+﻿using RPGSimulator.Common.Contracts;
+using RPGSimulatorCommon.Character.Jobs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestBot
+{
+    public class BotEmanuel : IBot
+    {
+        public IJob ChooseJob(IJobFactory jobFactory)
+        {
+            return jobFactory.CreateJob(JobType.Mage);
+        }
+
+        public void DoTurn(IGameController game)
+        {
+            Console.WriteLine("Emanuel is pro!");
+        }
+    }
+}
