5fe57e5718fc8a62439d3240258d66b62a6d4b01
FreeMotion45 <thewebsiter@gmail.com>
2020-01-17 17:41:34 +0200

    Switched in Ctor

--- RPGSimulator/RPGSimulator.Core/Abstractions/SkillBase.cs
+++ RPGSimulator/RPGSimulator.Core/Abstractions/SkillBase.cs
@@ -3,23 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RPGSimulator.Common.Character.Jobs.Skills;
 using RPGSimulator.Common.Contracts;
 using RPGSimulator.Core.Modules;
-using RPGSimulatorCommon.Character.Jobs.Skills;
 
 namespace RPGSimulator.Core.Abstractions
 {
     public abstract class SkillBase : ISkill
     {
         protected Random Random;
-        protected SkillBase(string name, string description)
+        protected SkillBase(SkillName name, string description)
         {
             Name = name;
             Description = description;
             Random = new Random();
         }
 
-        public string Name { get; }
+        public SkillName Name { get; }
 
         public string Description { get; }
 
